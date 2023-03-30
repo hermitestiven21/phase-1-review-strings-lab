a05d8764c8afaf225d3c7ea9654d42e99a174ade
MelissaStiven <stivenhermite@gmail.com>
2023-03-29 21:53:31 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code in this file!
+const currentUser = 'Melissa Stiven'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+
+
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`
+
+
+
+
